82b58f90e693d07a68ce7769b4e0c28164264b61
Brethren Ministry <brethrenmissionministry@gmail.com>
2026-07-21 12:06:47 +0300

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("welcomeButton");
+
+button.onclick = function(){
+
+document.getElementById("message").textContent =
+"Thank you for visiting Brethren Mission Ministry.";
+
+}
