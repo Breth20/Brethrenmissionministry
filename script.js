3baf403af5befc623b94d3fb17d887b69c2761e1
Brethren Ministry <brethrenmissionministry@gmail.com>
2026-07-22 14:28:48 +0300

    update

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ contactButton.onclick = function () {
         contactButton.textContent = "Our Contacts";
     }
 };
+let menuButton = document.getElementById("menuButton");
+let mobileMenu = document.getElementById("mobileMenu");
+
 menuButton.onclick = function () {
     mobileMenu.classList.toggle("show");
-    console.log(mobileMenu.className);
 };
