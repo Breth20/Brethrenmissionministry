985f9f7bd47a2e6fb334f81bae7e0954c5757d0e
Brethren Ministry <brethrenmissionministry@gmail.com>
2026-07-23 16:58:05 +0300

    update

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 let welcomeButton = document.getElementById("welcomeButton");
-welcomeButton.onclick = function() {
 
-    document.getElementById("message").textContent =
-    "Thank you for visiting Brethren Mission Ministry.";
-};
-
+if (welcomeButton) {
+    welcomeButton.onclick = function () {
+        document.getElementById("message").textContent =
+        "Thank you for visiting Brethren Mission Ministry.";
+    };
+}
 let contactButton = document.getElementById("contactButton");
-contactButton.onclick = function () {
 
-    if (contactButton.textContent === "Our Contacts") {
-        contactButton.textContent = "+255 719 745 738";
-    } else {
-        contactButton.textContent = "Our Contacts";
-    }
-};
+if (contactButton) {
+
+    contactButton.onclick = function () {
+
+        if (contactButton.textContent === "Our Contacts") {
+            contactButton.textContent = "+255 719 745 738";
+        } else {
+            contactButton.textContent = "Our Contacts";
+        }
+
+    };
+
+}
 let menuButton = document.getElementById("menuButton");
 let mobileMenu = document.getElementById("mobileMenu");
 
-menuButton.onclick = function () {
-    mobileMenu.classList.toggle("show");
-};
-
+if (menuButton && mobileMenu) {
+    menuButton.onclick = function () {
+        mobileMenu.classList.toggle("show");
+    };
+}
 let ministryButton = document.getElementById("ministryButton");
 
 let ministryMenu = document.getElementById("ministryMenu");
