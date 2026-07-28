8378a824a0eaabc087ae77503ce4e58689e931ed
Brethren Ministry <brethrenmissionministry@gmail.com>
2026-07-28 17:11:25 +0300

    update

--- script.js
+++ script.js
@@ -31,19 +31,19 @@ animatedItems.forEach(function(item){
 observer.observe(item);
 });
 
-const backToTop = document.getElementById("backToTop");
-if(backToTop){
+
+let backToTop = document.getElementById("backToTop");
 window.addEventListener("scroll", function(){
-if(window.scrollY > 300){
-backToTop.style.display = "block";
-}else{
-backToTop.style.display = "none";
-}
+    if(window.scrollY > 300){
+        backToTop.style.display = "flex";
+    }
+    else{
+        backToTop.style.display = "none";
+    }
 });
-backToTop.addEventListener("click", function(){
-window.scrollTo({
-top:0,
-behavior:"smooth"
-});
-});
-}
+backToTop.onclick = function(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+};
