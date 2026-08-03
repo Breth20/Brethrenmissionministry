3d85d7249e3c99b20371b90cb8ef310bf7253791
Brethren Ministry <brethrenmissionministry@gmail.com>
2026-08-03 15:50:15 +0300

    update

--- script.js
+++ script.js
@@ -302,3 +302,22 @@ console.log(document.getElementById("mobileLanguageSelect"));
 setupLanguageSwitcher("languageSelect");
 setupLanguageSwitcher("mobileLanguageSelect");
 
+// =======================================
+// BACK TO TOP BUTTON
+// =======================================
+const backToTop = document.getElementById("backToTop");
+if(backToTop){
+window.addEventListener("scroll",function(){
+if(window.scrollY > 300){
+backToTop.style.display = "block";
+}else{
+backToTop.style.display = "none";
+}
+});
+backToTop.addEventListener("click",function(){
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+});
+}
